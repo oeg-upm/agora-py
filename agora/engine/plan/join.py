a86f8a9fe46b288399729fbab824fb5a750b57c0
fserena <fserena@fi.upm.es>
2017-05-16 13:26:42 +0200

    Optimized path joins.

--- agora/engine/plan/join.py
+++ agora/engine/plan/join.py
@@ -20,9 +20,11 @@
 """
 import logging
 
-from rdflib import BNode
+from rdflib import BNode, Graph
 from rdflib import Literal
 from rdflib import RDF
+
+from agora import Fountain
 
 __author__ = 'Fernando Serena'
 
@@ -41,6 +43,11 @@ def _stringify_tp(context, (s, p, o)):
     return '{} {} {} .'.format(stringify_elm(s), stringify_elm(p), stringify_elm(o))
 
 
+def type_hierarchy(fountain, ty):
+    type_dict = fountain.get_type(ty)
+    return set([ty] + type_dict['sub'])
+
+
 def subject_join(fountain, tp_paths, context, tp1, tp2, **kwargs):
     # type: (Fountain, iter, Graph, tuple, tuple, dict) -> iter
     subject, pr1, o1 = tp1
@@ -49,11 +56,11 @@ def subject_join(fountain, tp_paths, context, tp1, tp2, **kwargs):
     if pr2 == RDF.type:
         o2 = context.qname(o2)
         tp2_domain = [o2]
-        tp2_domain.extend(fountain.get_type(o2).get('sub'))
+        tp2_domain.extend(fountain.get_type(o2).get('super'))
     else:
         tp2_domain = fountain.get_property(context.qname(pr2)).get('domain')
 
-    join_paths = tp_paths[tp1][:]
+    filter_paths = tp_paths[tp1][:]
 
     if pr1 == RDF.type:
         for path in tp_paths[tp1]:
@@ -62,20 +69,28 @@ def subject_join(fountain, tp_paths, context, tp1, tp2, **kwargs):
                 last_prop = path.get('steps')[-1].get('property')
                 dom_r = fountain.get_property(last_prop).get('range')
                 if len(filter(lambda x: x in tp2_domain, dom_r)):
-                    join_paths.remove(path)
+                    filter_paths.remove(path)
             else:
-                join_paths.remove(path)
+                filter_paths.remove(path)
     elif pr2 == RDF.type:
         for path in tp_paths[tp1]:
             last_type = path.get('steps')[-1].get('type')
-            if last_type in tp2_domain:
-                join_paths.remove(path)
+            types = type_hierarchy(fountain, last_type)
+            if set.intersection(types, tp2_domain):
+                filter_paths.remove(path)
     else:
         for path in tp_paths[tp1]:
-            if path.get('steps')[-1].get('type') in tp2_domain:
-                join_paths.remove(path)
+            steps = path.get('steps', [])
+            if len(steps):
+                matching_steps = steps[:-1]
+                for o_path in tp_paths[tp2]:
+                    o_steps = o_path.get('steps', [])
+                    if len(o_steps):
+                        o_matching_steps = o_steps[:-1]
+                        if matching_steps == o_matching_steps and path in filter_paths:
+                            filter_paths.remove(path)
 
-    return join_paths
+    return filter_paths
 
 
 def subject_object_join(fountain, tp_paths, context, tp1, tp2, hints=None, cycles=None):
@@ -84,7 +99,7 @@ def subject_object_join(fountain, tp_paths, context, tp1, tp2, hints=None, cycle
     _, pr2, o2 = tp2
 
     pr2 = context.qname(pr2)
-    join_paths = tp_paths[tp1][:]
+    filter_paths = tp_paths[tp1][:]
 
     if pr1 == RDF.type or subject == o2:
         for path in tp_paths[tp1]:
@@ -95,8 +110,8 @@ def subject_object_join(fountain, tp_paths, context, tp1, tp2, hints=None, cycle
                 else:
                     matching_steps = steps[:-1]
                 for o_path in tp_paths[tp2]:
-                    if o_path.get('steps') == matching_steps and path in join_paths:
-                        join_paths.remove(path)
+                    if o_path.get('steps') == matching_steps and path in filter_paths:
+                        filter_paths.remove(path)
     elif pr2 == context.qname(RDF.type):
         tp1_range = fountain.get_property(context.qname(pr1)).get('range')
         o2 = context.qname(o2)
@@ -104,21 +119,23 @@ def subject_object_join(fountain, tp_paths, context, tp1, tp2, hints=None, cycle
             check_types = fountain.get_type(r_type).get('super')
             check_types.append(r_type)
             if o2 in check_types:
-                join_paths = []
+                filter_paths = []
                 break
-        if not join_paths and hints is not None:
+        if not filter_paths and hints is not None:
             hints[tp2]['check'] = hints[tp2].get('check', False) or len(tp1_range) > 1
     else:
         if not subject == o2:
             for path in tp_paths[tp1]:
-                steps = path.get('steps', [])
-                if len(steps):
-                    subject_prop = steps[-1].get('property')
-                    subject_range = fountain.get_property(subject_prop).get('range')
-                    for join_subject in subject_range:
-                        if pr2 in fountain.get_type(join_subject).get('properties') and path in join_paths:
-                            join_paths.remove(path)
-    return join_paths
+                for o_path in tp_paths[tp2]:
+                    steps = o_path.get('steps', [])
+                    if len(steps):
+                        if pr2 == RDF.type:
+                            matching_steps = steps[:]
+                        else:
+                            matching_steps = steps[:-1]
+                        if path.get('steps') == matching_steps and path in filter_paths:
+                            filter_paths.remove(path)
+    return filter_paths
 
 
 def object_join(fountain, tp_paths, context, tp1, tp2, **kwargs):
