e5658ac7025d31bc4f70e13baef325730c7b22d0
fserena <fserena@fi.upm.es>
2017-06-14 20:38:35 +0200

    Agora uses __new__ for creation and dependency injection

--- agora/__init__.py
+++ agora/__init__.py
@@ -20,17 +20,41 @@
 """
 import logging
 
+from agora.engine.fountain.seed import SeedManager
+
+from agora.engine.fountain.path import PathManager
+
+from agora.engine.utils.kv import get_kv, close
+
+from agora.engine.utils.graph import get_cached_triple_store
+
+from agora.engine.fountain.index import Index
+
+from agora.engine.fountain.schema import Schema
+
 from agora.collector import Collector
 from agora.collector.cache import RedisCache
 from agora.engine.fountain import Fountain
-from agora.engine.plan import Planner
+from agora.engine.plan import Planner, AbstractPlanner
 from agora.graph import AgoraGraph
 from agora.server.fountain import FountainClient
 from agora.server.fountain import client as fc
 from agora.server.planner import client as pc
+from agora.engine.utils import stopped
 from rdflib import Graph
+import signal
+import sys
 
 __author__ = 'Fernando Serena'
+
+
+def signal_term_handler(signal, frame):
+    print 'got SIGTERM'
+    Agora.close()
+    # sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, signal_term_handler)
 
 
 def setup_logging(level):
@@ -45,32 +69,41 @@ def setup_logging(level):
 
 class Agora(object):
     def __init__(self, **kwargs):
-        self._fountain = Fountain(**kwargs)
-        self._planner = Planner(self._fountain)
+        pass
 
     @property
     def fountain(self):
-        return self._fountain
+        return self.planner.fountain
 
     @property
     def planner(self):
+        # type: () -> AbstractPlanner
         return self._planner
 
-    def __get_agora_graph(self, collector, cache, loader):
-        collector = collector if collector is not None else Collector(self._planner, cache=cache)
+    @planner.setter
+    def planner(self, p):
+        self._planner = p
+
+    def __get_agora_graph(self, collector, cache, loader, force_seed):
+        if collector is None:
+            collector = Collector()
+            collector.planner = self.planner
+            collector.cache = cache
         if loader is not None:
             collector.loader = loader
+        if force_seed is not None:
+            collector.force_seed = force_seed
         return AgoraGraph(collector)
 
-    def query(self, query, collector=None, cache=None, loader=None, **kwargs):
-        graph = self.__get_agora_graph(collector, cache, loader)
+    def query(self, query, collector=None, cache=None, loader=None, force_seed=None, **kwargs):
+        graph = self.__get_agora_graph(collector, cache, loader, force_seed)
         return graph.query(query, collector=collector, **kwargs)
 
-    def fragment(self, query=None, agps=None, collector=None, cache=None, loader=None):
+    def fragment(self, query=None, agps=None, collector=None, cache=None, loader=None, force_seed=None):
         if not (query or agps):
             return
 
-        graph = self.__get_agora_graph(collector, cache, loader)
+        graph = self.__get_agora_graph(collector, cache, loader, force_seed)
         result = Graph(namespace_manager=graph.namespace_manager)
 
         agps = list(graph.agps(query)) if query else agps
@@ -80,13 +113,13 @@ class Agora(object):
                 result.add((s, p, o))
         return result
 
-    def fragment_generator(self, query=None, agps=None, collector=None, cache=None, loader=None):
+    def fragment_generator(self, query=None, agps=None, collector=None, cache=None, loader=None, force_seed=None):
         def comp_gen(gens):
             for gen in [g['generator'] for g in gens]:
                 for q in gen:
                     yield q
 
-        graph = self.__get_agora_graph(collector, cache, loader)
+        graph = self.__get_agora_graph(collector, cache, loader, force_seed)
         agps = list(graph.agps(query)) if query else agps
 
         generators = [graph.collector.get_fragment_generator(agp, filters=filters) for agp, filters in agps]
@@ -98,15 +131,46 @@ class Agora(object):
 
         return {'prefixes': prefixes, 'plan': comp_plan, 'generator': comp_gen(generators), 'gens': generators}
 
-    def search_plan(self, query):
-        collector = Collector(self._planner)
+    def search_plan(self, query, force_seed=None):
+        collector = Collector()
+        collector.planner = self.planner
         graph = AgoraGraph(collector)
         comp_plan = Graph(namespace_manager=graph.namespace_manager)
         for agp, filters in graph.agps(query):
-            comp_plan.__iadd__(self._planner.make_plan(agp))
+            comp_plan.__iadd__(self._planner.make_plan(agp, force_seed))
         return comp_plan
 
     def agp(self, query):
-        collector = Collector(self._planner)
+        collector = Collector()
+        collector.planner = self.planner
         graph = AgoraGraph(collector)
         return graph.agps(query)
+
+    def __new__(cls, **kwargs):
+        a = super(Agora, cls).__new__(cls)
+        kv = get_kv(**kwargs)
+        schema = Schema()
+        schema.graph = get_cached_triple_store(schema.cache, **kwargs)
+        index = Index()
+        index.r = kv
+        index.schema = schema
+        sm = SeedManager()
+        sm.index = index
+        pm = PathManager()
+        pm.index = index
+        pm.seed_manager = sm
+
+        fountain = Fountain()
+        fountain.schema = schema
+        fountain.index = index
+        fountain.seed_manager = sm
+        fountain.path_manager = pm
+        planner = Planner(fountain)
+        a.planner = planner
+
+        return a
+
+    @staticmethod
+    def close():
+        close()
+        stopped.set()
